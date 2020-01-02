8e739abfa277f2302f34294b96b449fa38ab3a8d
sunyijun <sunyijun@navercorp.com>
2020-01-02 10:43:22 +0800

    [feat] support all IDEA products
    remove apache io dependence

--- git-commit-template-plugin-idea/src/yijun/sun/plugin/idea/UseGitCommitTemplateMsg.java
+++ git-commit-template-plugin-idea/src/yijun/sun/plugin/idea/UseGitCommitTemplateMsg.java
@@ -1,10 +1,11 @@
 package yijun.sun.plugin.idea;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Objects;
-
-import org.apache.commons.io.FileUtils;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
@@ -30,18 +31,43 @@ public class UseGitCommitTemplateMsg extends AnAction {
 			return;
 		}
 		File file = new File(project.getBasePath(), "git-message-template.txt");
-		if (!file.exists()) {
-			try {
-				FileUtils.writeStringToFile(file, message);
-			} catch (IOException e1) {
-				e1.printStackTrace();
+		try {
+			if (!file.exists()) {
+				boolean createResult = file.createNewFile();
+				if (createResult) {
+					FileWriter writer = null;
+					try {
+						writer = new FileWriter(file);
+						writer.write(message);
+						writer.flush();
+					} catch (Throwable ex) {
+						ex.printStackTrace();
+						if (writer != null) {
+							writer.close();
+						}
+					}
+				}
+			} else if (file.isFile() && file.canRead()) {
+				FileReader reader = null;
+				BufferedReader buffer = null;
+				try {
+					reader = new FileReader(file);
+					buffer = new BufferedReader(reader);
+					final StringBuilder s = new StringBuilder();
+					buffer.lines().forEach(l -> s.append(l).append("\n"));
+					message = s.toString();
+				} catch (Throwable ex) {
+					if (buffer != null) {
+						buffer.close();
+					}
+					if (reader != null) {
+						reader.close();
+					}
+					ex.printStackTrace();
+				}
 			}
-		} else {
-			try {
-				message = FileUtils.readFileToString(file);
-			} catch (IOException e1) {
-				e1.printStackTrace();
-			}
+		} catch (IOException e1) {
+			e1.printStackTrace();
 		}
 
 		commitMessage.setCommitMessage(message);
@@ -50,7 +76,7 @@ public class UseGitCommitTemplateMsg extends AnAction {
 	private String getDefaultMessage() {
 		return "[type] subject\n"
 			+ "# Please remove comments. \n"
-			+ "# type: feat(new feature) / oss(oss bug) / bts(bts bug) / docs / style(file style change) / refactor / test(only test code change) / chore(build or tool)\n"
+			+ "# type: feat(new feature) / fix / docs / style(file style change) / refactor / test(only test code change) / chore(build or tool)\n"
 			+ "\n"
 			+ "Issues: \n"
 			+ "Description: \n"
